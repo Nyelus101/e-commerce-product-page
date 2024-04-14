d168ab6f05687cc3ffc91c4720b64460213d3963
Nyelus101 <cmuonyelu@gmail.com>
2024-04-14 09:34:03 +0100

    update

--- E-commerce-page/src/components/Nav.jsx
+++ E-commerce-page/src/components/Nav.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from "../images/logo.svg";
 import { AiOutlineShoppingCart, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import avatar from "../images/image-avatar.png";
+import Cart from './Cart';
 
 const Nav = () => {
 
@@ -11,17 +12,35 @@ const Nav = () => {
     setNav(!nav);
   };
 
+  const [cartIsOpen, setCartIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Set nav to false when the screen size is above a certain breakpoint
+      if (window.innerWidth >= 768) {
+        setNav(false);
+      }
+    };
+
+    // Listen for window resize events
+    window.addEventListener('resize', handleResize);
+
+    // Remove the event listener when component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
-      <div className='flex items-center justify-between p-8 md:border-b border-slate-400 max-w-5xl mx-auto bg-red-500'>
+      <div className='flex items-center justify-between p-8 md:border-b border-slate-400 max-w-5xl mx-auto'>
       <div onClick={handleNav} className='-mr-32 block md:hidden relative z-50'>
           {nav ? <AiOutlineClose size={20} className="relative z-50"/> : <AiOutlineMenu size={20} className="relative z-50"/>}
       </div>
       <div className='flex items-center justify-start gap-4'>
         <img src={logo} alt="" className='p-0'/>
         <nav>
-          {/* <ul className={` ${nav ? 'block absolute left-0 top-0 w-[60%] h-full bg-white pt-32 pl-8 space-y-4' : 'hidden md:flex items-center justify-start gap-4'}`}> */}
-          <ul className={` ${nav ? 'block absolute left-0 top-0 w-[60%] h-full bg-white pt-32 pl-8 space-y-4' : 'hidden md:flex items-center justify-start gap-4'}`}>
+          <ul className={`${nav ? 'block absolute left-0 top-0 w-[60%] h-full bg-white pt-32 pl-8 space-y-4' : '  hidden md:flex'}  items-center justify-start gap-4`}>
             <li>Collections</li>
             <li>Men</li>
             <li>Women</li>
@@ -32,9 +51,10 @@ const Nav = () => {
       </div>
       <div>
         <ul className='flex items-center justify-start gap-4'>
-          <li><button>
-                <AiOutlineShoppingCart />
+          <li><button onClick={() => setCartIsOpen(!cartIsOpen)}>
+                <AiOutlineShoppingCart className="text-2xl text-slate-600" />
               </button></li>
+              <li>{cartIsOpen && <Cart />}</li>
           <li>
             <img src={avatar} alt="" className='w-10
             '/>
